58e810f5ee753ac3a11b55874ac42b0b4077ba50
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2022-01-13 15:46:03 +0100

    improve the construction of the proof

--- apps/derive/tests/test_eqbcorrect.v
+++ apps/derive/tests/test_eqbcorrect.v
@@ -13,8 +13,8 @@ Import test_derive_stdlib.Coverage
     
 Module Coverage.
 
-(* Fail Elpi derive.eqbcorrect empty.  (* No induction principle *) *)
-Elpi Trace "derive.eqbcorrect.*" (* "derive.param1.functor.*" *) "correct-lemma-for" "param1-functor-for". 
+(* Elpi Trace (* "derive.eqbcorrect.*" "derive.param1.functor.*" "correct-lemma-for" *) "param1-functor-for". *)
+Elpi derive.eqbcorrect empty.  (* No induction principle *) 
 
 Elpi derive.eqbcorrect unit. 
 
@@ -25,13 +25,13 @@ Elpi derive.eqbcorrect pair.
 Elpi derive.eqbcorrect seq.
 
 Elpi derive.eqbcorrect rose.
-(*
-Fail Elpi derive.eqbcorrect nest.
-Fail Elpi derive.eqbcorrect w.
-Fail Elpi derive.eqbcorrect vect.
-Fail Elpi derive.eqbcorrect dyn.
-Fail Elpi derive.eqbcorrect zeta.
-Fail Elpi derive.eqbcorrect beta.
+
+Fail Elpi derive.eqbcorrect nest. (* Maybe fixable *)
+Fail Elpi derive.eqbcorrect w.    (* Not fixable *)
+Fail Elpi derive.eqbcorrect vect. (* Can be done *)
+Fail Elpi derive.eqbcorrect dyn.  (* Not Fixable *)
+Fail Elpi derive.eqbcorrect zeta. (* FIXME *)
+Elpi derive.eqbcorrect beta. 
 Fail Elpi derive.eqbcorrect iota.
 Fail Elpi derive.eqbcorrect prim_int.
 Fail Elpi derive.eqbcorrect prim_float.
@@ -40,8 +40,8 @@ Fail Elpi derive.eqbcorrect pa_record.
 Fail Elpi derive.eqbcorrect pr_record. 
 Fail Elpi derive.eqbcorrect dep_record.
 Elpi derive.eqbcorrect enum.
-Fail Elpi derive.eqbcorrect sigma_bool.
-*)
+Fail Elpi derive.eqbcorrect sigma_bool. (* A lot is missing for this one *)
+
 End Coverage.
 Import Coverage.
 Import PArith.
